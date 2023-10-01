92eea88a587ecc595b1cd6c5efc5009a8c6fb8cf
icediver <icedivet@live.ru>
2023-10-01 12:47:50 +0300

    feature(media) --add media controller for uploads files

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 import { MediaService } from './media.service';
 
 @Controller('media')
 export class MediaController {
-	constructor(private readonly mediaService: MediaService) {}
+  constructor(private readonly mediaService: MediaService) {}
+
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadsMediaFile(
+    @UploadedFile() mediaFile: Express.Multer.File,
+    @Query('folder') folder?: string
+  ) {
+    console.log(mediaFile, folder);
+    console.log('nne');
+
+    return this.mediaService.saveMedia(mediaFile, folder);
+  }
 }
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+import { path } from 'app-root-path';
+import { IMediaResponse } from './media.interface';
+import { ensureDir, writeFile } from 'fs-extra';
 @Injectable()
-export class MediaService {}
+export class MediaService {
+  async saveMedia(
+    mediaFile: Express.Multer.File,
+    folder = 'media'
+  ): Promise<IMediaResponse> {
+    const uploadFolder = `${path}/uploads/${folder}`;
+    await ensureDir(uploadFolder);
+
+    await writeFile(
+      `${uploadFolder}/${mediaFile.originalname}`,
+      mediaFile.buffer
+    );
+
+    return {
+      url: `/uploads/${folder}/${mediaFile.originalname}`,
+      name: mediaFile.originalname
+    };
+  }
+}
